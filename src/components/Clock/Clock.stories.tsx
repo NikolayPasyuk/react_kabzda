af7ecb5016fcd917c7b237b7a88a1d77500765aa
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-16 19:12:18 +0300

    added getHours,getMinutes,getSeconds

--- src/components/Clock/Clock.stories.tsx
+++ src/components/Clock/Clock.stories.tsx
@@ -2,14 +2,14 @@ import React, {useState} from 'react';
 
 type PropsType = {}
 
-const Clock: React.FC<PropsType> = (props) => {
+export const Clock: React.FC<PropsType> = (props) => {
     const [date, setDate] = useState(new Date())
 
     return <div>
-        <span></span>
+        <span>{date.getHours()}</span>
         :
-        <span></span>
+        <span>{date.getMinutes()}</span>
         :
-        <span></span>
+        <span>{date.getSeconds()}</span>
     </div>
 }
