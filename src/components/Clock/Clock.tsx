4904c7fe08cf2ab075e40fb5a29c00e90500e221
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-16 20:04:30 +0300

    created function get2digitsString

--- src/components/Clock/Clock.tsx
+++ src/components/Clock/Clock.tsx
@@ -11,17 +11,20 @@ export const Clock: React.FC<PropsType> = (props) => {
         }, 1000)
     }, [])
 
+    const get2digitsString = (num: number) => num < 10 ? '0' + num : num
+
+
     const secondsString = date.getSeconds() < 10
         ? '0' + date.getSeconds()
         : date.getSeconds()
 
     const minutesString = date.getMinutes() < 10
         ? '0' + date.getMinutes()
-        : date.getSeconds()
+        : date.getMinutes()
 
     const hoursString = date.getHours() < 10
         ? '0' + date.getHours()
-        : date.getSeconds()
+        : date.getHours()
 
     return <div>
         <span>{hoursString}</span>
