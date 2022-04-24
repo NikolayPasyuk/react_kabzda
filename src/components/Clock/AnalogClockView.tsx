7acba3f64c76370588bc5562813f8043195d3b34
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-24 15:32:19 +0300

    added className for component AnalogClockView

--- src/components/Clock/AnalogClockView.tsx
+++ src/components/Clock/AnalogClockView.tsx
@@ -2,5 +2,11 @@ import React from 'react';
 import {ClockViewPropsType} from './Clock';
 
 export const AnalogClockView: React.FC<ClockViewPropsType> = ({date}) => {
-    return <span>ANALOG</span>
+    return <div className={'clock'}>
+        <div className={'analog-clock'}>
+            <div className={'dial seconds'}/>
+            <div className={'dial minutes'}/>
+            <div className={'dial hours'}/>
+        </div>
+    </div>
 }
