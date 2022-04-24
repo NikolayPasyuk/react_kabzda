ce1c253d9bc8dd371f7fb0eec6c3adc634f676f8
NikolayPasyuk <kolyan-1812@mail.ru>
2022-04-24 15:27:48 +0300

    moved the get2digitsString function to the DigitalClockView file

--- src/components/Clock/DigitalClockView.tsx
+++ src/components/Clock/DigitalClockView.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import {ClockViewPropsType, get2digitsString} from './Clock';
+import {ClockViewPropsType} from './Clock';
+
+export const get2digitsString = (num: number) => num < 10 ? '0' + num : num
 
 export const DigitalClockView: React.FC<ClockViewPropsType> = ({date}) => {
     return <><span>{get2digitsString(date.getHours())}</span>
